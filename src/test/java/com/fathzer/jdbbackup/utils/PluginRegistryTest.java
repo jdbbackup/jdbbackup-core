d51f013e406d12357bb8b719761ba570dee6f0ae
Fathzer <jm-dev@astesana.net>
2023-03-14 16:18:35 +0100

    More tests

--- src/test/java/com/fathzer/jdbbackup/utils/PluginRegistryTest.java
+++ src/test/java/com/fathzer/jdbbackup/utils/PluginRegistryTest.java
@@ -12,25 +12,36 @@ import com.fathzer.jdbbackup.dumpers.MySQLDumper;
 
 class PluginRegistryTest {
 
+	private static final class FakePlugin implements DBDumper {
+		private String scheme;
+
+		private FakePlugin(String scheme) {
+			super();
+			this.scheme = scheme;
+		}
+
+		@Override
+		public void save(String source, File destFile) throws IOException {
+			//Fake plugin
+		}
+
+		@Override
+		public String getScheme() {
+			return scheme;
+		}
+	}
+
 	@Test
 	void test() {
 		PluginRegistry<DBDumper> registry = new PluginRegistry<>(DBDumper.class, DBDumper::getScheme);
 		assertTrue(registry.load(ClassLoader.getSystemClassLoader()));
 		assertFalse(registry.load(ClassLoader.getSystemClassLoader()));
 		assertFalse(registry.register(new MySQLDumper()));
-		assertTrue(registry.register(new DBDumper() {
-			
-			@Override
-			public void save(String source, File destFile) throws IOException {
-				//Fake plugin
-			}
-			
-			@Override
-			public String getScheme() {
-				return "test";
-			}
-		}));
+		assertNull(registry.get("test"));
+		assertTrue(registry.register(new FakePlugin("test")));
 		assertNotNull(registry.get("test"));
+		assertFalse(registry.register(new FakePlugin("test")));
+		assertTrue(registry.register(new FakePlugin(new MySQLDumper().getScheme())));
 	}
 
 }
